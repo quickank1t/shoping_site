11399119870387e6043fad4b4440b27dd20d26a6
AC <a.chaurasia@sap.com>
2023-09-02 10:50:07 +0200

    env 🚀

--- server/lib/config.js
+++ server/lib/config.js
@@ -5,6 +5,10 @@ class Config {
     dotenv.config();
   }
 
+  get logLevel() {
+    return process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "INFO";
+  }
+
   get port() {
     return process.env.PORT ? process.env.PORT : 3000;
   }
