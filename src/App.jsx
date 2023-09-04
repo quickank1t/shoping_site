686db8e6e02638a26c0dbaaf0c9d68091e5a746d
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-04 20:55:03 +0530

    feat : React Router Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import Signup from "./Components/Signup";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Cart from "./Components/Cart";
+import Error from "./Components/Error";
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 function App() {
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			errorElement: <Error />,
+			element: <Body />,
+		},
+		{
+			path: "/signup",
+			element: <Signup />,
+		},
+		{
+			path: "/cart",
+			element: <Cart />,
+		},
+	]);
 	return (
 		<div>
 			<Header />
-			<Body />
+			<RouterProvider router={router} />
 			<Footer />
 		</div>
 	);
