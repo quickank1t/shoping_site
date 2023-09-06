7733e0792e670d83035c34f68d41b609ec617721
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2023-09-06 23:41:39 +0530

    feat : Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Body from "./Components/Body";
+import Signup from "./Components/Signup";
 function App() {
-  return <>test</>;
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Body />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
+  return (
+    <div>
+      <Header />
+      <RouterProvider router={router} />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
